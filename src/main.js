356a3791031388413f096bc4833fbd81b052d0ea
azizsige <kurniawan00azizsige@gmail.com>
2023-02-07 20:02:04 +0700

    add: pinia

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import "./style.css";
 import "flowbite/dist/flowbite.min.js";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 
 import router from "./router.js";
+const pinia = createPinia();
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(pinia).mount("#app");
